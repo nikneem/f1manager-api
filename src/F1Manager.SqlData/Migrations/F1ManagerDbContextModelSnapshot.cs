97311f8d3ac62d9ba86ab4266b15b822f4c98ad6
Eduard <e.keilholz@4dotnet.nl>
2022-03-16 14:37:14 +0100

    Adding migration

--- src/F1Manager.SqlData/Migrations/F1ManagerDbContextModelSnapshot.cs
+++ src/F1Manager.SqlData/Migrations/F1ManagerDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace F1Manager.SqlData.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.1")
+                .HasAnnotation("ProductVersion", "6.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
